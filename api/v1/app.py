aeb86faa1394ff80194c82d7f7770ab94f3c2fbf
Adriana Echeverri <adriana.er98@hotmail.com>
2021-09-18 17:02:30 +0000

    [STYLE] (pep8 styling)

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """ Tears down the session """
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@ from api.v1.views import app_views
 from flask import jsonify
 """ Routes related to app_views blueprint """
 
+
 @app_views.route('/status')
 def show_status():
     """ Shows status of an endpoint. """
