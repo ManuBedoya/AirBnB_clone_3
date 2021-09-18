66025c018f0195cd9b426c2cb96990549cd4f95a
Adriana Echeverri <adriana.er98@hotmail.com>
2021-09-18 18:32:19 +0000

    [FEATURE] (Stats, counts objects)

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,24 @@ from flask import jsonify
 def show_status():
     """ Shows status of an endpoint. """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def gives_stats():
+    """ Counts the amount of objects of each class. """
+    from models.engine.db_storage import classes
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    dict_count = {
+        "amenities": storage.count(Amenity), 
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(dict_count)
