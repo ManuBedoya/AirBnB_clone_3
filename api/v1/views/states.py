209c4b7397e303ddbcc1558130c0067fd67a22f6
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-18 19:23:33 -0500

    [view_States](Adding method PUT and POST)

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -19,10 +19,10 @@ def display_states(state_id=None):
             states = storage.get(State, state_id).to_dict()
         except:
             abort(404)
- 
+
     return jsonify(states)
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_states(state_id):
     """ Deletes a state """
     try:
@@ -30,5 +30,36 @@ def delete_states(state_id):
         storage.delete(state)
         storage.save()
         return jsonify({}), 200
+    except:
+        abort(404)
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_states(state_id):
+    """ Create new state """
+    try:
+        if not request.get_json():
+            return jsonify({'error': 'Not a JSON'}), 400
+        if name not in request.get_json():
+            return jsonify({'error': 'Missing name'}), 400
+
+        state = State(**request.get_json())
+        state.save()
+        return jsonify({state}), 201
+    except:
+        abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_states(state_id):
+    """ update state """
+    try:
+        state = storage.get(State, state_id)
+        if not request.get_json():
+            return jsonify({'error': 'Not a JSON'}), 400
+        for key, value in request.get_json().items():
+            if key not in State.__dict__.keys():
+                setattr(state, key, value)
+        state.save()
+        return jsonify({state}), 201
     except:
         abort(404)
