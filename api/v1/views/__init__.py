e8edf712d720d476d7170653bc1f00247ff53f11
root <root@manuel-PC>
2021-09-20 12:46:06 -0500

    [FEATURES](Add the cores)

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Creates blueprint app_views """
 from flask import Blueprint
+from flask_cors import CORS
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+CORS(app_views, resources={r"/api/*":{"origins":"0.0.0.0"}})
 
 from api.v1.views.index import *
 from api.v1.views.states import *
